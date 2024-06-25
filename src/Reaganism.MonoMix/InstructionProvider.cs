8b5b399d50e3b356a7b14de46de08514bc4b8462
tomat <xxlennygamerxx@gmail.com>
2024-06-24 22:56:37 -0700

    Document utilities in InstructionProvider.

--- src/Reaganism.MonoMix/InstructionProvider.cs
+++ src/Reaganism.MonoMix/InstructionProvider.cs
@@ -14,28 +14,53 @@ namespace Reaganism.MonoMix;
 ///     objects.
 /// </summary>
 public static class InstructionProvider {
+    /// <summary>
+    ///     Gets the instructions of a <see cref="MethodDefinition"/>.
+    /// </summary>
     public static IEnumerable<Instruction> FromMethodDefinition(MethodDefinition methodDefinition) {
         return FromMethodBody(methodDefinition.Body);
     }
 
+    /// <summary>
+    ///     Gets the instructions of an <see cref="ILContext"/>.
+    /// </summary>
     public static IEnumerable<Instruction> FromILContext(ILContext context) {
         return FromMethodBody(context.Body);
     }
 
+    /// <summary>
+    ///     Gets the instructions of an <see cref="ILCursor"/>.
+    /// </summary>
     public static IEnumerable<Instruction> FromILCursor(ILCursor cursor) {
         return FromMethodBody(cursor.Body);
     }
 
+    /// <summary>
+    ///     Gets the instructions of a <see cref="MethodBody"/>.
+    /// </summary>
     public static IEnumerable<Instruction> FromMethodBody(MethodBody methodBody) {
         return methodBody.Instructions;
     }
 
+    /// <summary>
+    ///     Gets the instructions of a <see cref="MethodBase"/> with operands
+    ///     mapped to Mono.Cecil types.
+    /// </summary>
+    /// <remarks>
+    ///     This method uses a <see cref="DynamicMethodDefinition"/> to handle
+    ///     disassembling the method, meaning one has to manually dispose of
+    ///     the instance (<paramref name="disposable"/>) when appropriate.
+    /// </remarks>
     public static IEnumerable<Instruction> FromMethodBaseAsCecil(MethodBase methodBase, out IDisposable disposable) {
         var dynDef = new DynamicMethodDefinition(methodBase);
         disposable = dynDef;
         return dynDef.Definition.Body.Instructions;
     }
 
+    /// <summary>
+    ///     Gets the instructions of a <see cref="MethodBase"/> with operands
+    ///     mapped to System types.
+    /// </summary>
     public static IEnumerable<Instruction> FromMethodBaseAsSystem(MethodBase methodBase) {
         return MethodBodyDisassembler.FromMethodBase(methodBase);
     }
